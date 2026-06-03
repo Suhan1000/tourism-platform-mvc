32363611cb6f3d62f0620141386b163cc673b8da
Suhan Bishwakarma <20029804@students.koi.edu.au>
2026-06-03 18:12:48 +1000

    Delete FinalTourWeb/TourismPlatform - Copy directory

--- FinalTourWeb/TourismPlatform - Copy/TourismPlatform/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TourismPlatform.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
