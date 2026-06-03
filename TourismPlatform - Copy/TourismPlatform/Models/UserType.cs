d728ca224e338519e1c2d8a9ae69b71032c981d4
Suhan Bishwakarma <20029804@students.koi.edu.au>
2026-06-03 18:12:35 +1000

    Delete TourismPlatform - Copy directory

--- TourismPlatform - Copy/TourismPlatform/Models/UserType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TourismPlatform.Models
-{
-    public enum UserType
-    {
-        TravelAgency,    // Companies with multiple tours and guides
-        TourGuide,       // Individual tour guides
-        Tourist          // Customers who book tours
-    }
-}
